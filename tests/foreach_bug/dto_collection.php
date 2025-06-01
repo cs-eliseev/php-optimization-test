1e368a4e4be3d6f334c30116c5f06389531a1041
ae <ae@usedesk.ru>
2025-06-01 21:25:32 +0300

    feat(2): add test foreach bug

---
+++ tests/foreach_bug/dto_collection.php
@@ -0,0 +1,30 @@
+<?php
+
+ini_set('memory_limit', '-1');
+
+$iterations = 10000000;
+
+function showTs(float $ts): void
+{
+    print(' ts: ' . str_replace('.', ',', (string)(microtime(true) - $ts)) . PHP_EOL);
+}
+
+class TestDTO
+{
+    public $attr_int;
+    public $attr_float;
+    public $attr_string;
+    public $attr_array;
+}
+
+$collection = [];
+for ($i = 0; $i < $iterations; $i++) {
+    $item = new TestDTO();
+    $item->attr_int = $i;
+    $collection[] = $item;
+}
+
+$ts = microtime(true);
+foreach ($collection as $item) {
+}
+showTs($ts);
