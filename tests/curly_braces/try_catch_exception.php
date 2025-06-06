5dac7fe546e65de40368a147a49a4bd64a851072
ae <ae@usedesk.ru>
2025-06-07 01:40:01 +0300

    feat(5): add test block code

---
+++ tests/curly_braces/try_catch_exception.php
@@ -0,0 +1,20 @@
+<?php
+
+ini_set('memory_limit', '-1');
+
+function showTs(float $ts): void
+{
+    print('ts: ' . str_replace('.', ',', (string)(microtime(true) - $ts)) . PHP_EOL);
+}
+
+$iterations = 50000000;
+
+$ts = microtime(true);
+for ($i = 0; $i < $iterations; $i++) {
+    try {
+        throw new Exception();
+    } catch (Throwable $exception) {
+        // do nothing
+    }
+}
+showTs($ts);
