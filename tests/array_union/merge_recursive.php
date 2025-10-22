9ab842f1ad14fc7410375b72955fca71d579affe
ae <ae@usedesk.ru>
2025-10-22 23:49:14 +0300

    feat(19): array union

---
+++ tests/array_union/merge_recursive.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once __DIR__.'/../utils.php';
+
+$iterations = COUNT_250_MILLIONS;
+
+$collection_1 = [];
+$collection_2 = [];
+for ($i = 0; $i < $iterations; $i++) {
+    $collection_1[] = $i;
+    $collection_2[] = $i;
+}
+
+runPerformanceTest(static function() use ($collection_1, $collection_2) {
+    $new = array_merge_recursive($collection_1, $collection_2);
+});
